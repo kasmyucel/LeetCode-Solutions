96b90ab9e67994f7e8add92aec134b3b6d466694
novCell <150459974+kasmyucel@users.noreply.github.com>
2024-11-29 10:34:03 +0300

    Create leetCode_climbingStairs.java

---
+++ leetCode_climbingStairs.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n == 1)
+          return 1;
+        else if(n==2)
+          return 2;
+        else
+          return fibonacciSequence(n);
+    }
+    public int fibonacciSequence(int termNum){
+        int lastTerm =1;
+        int tempTerm =1;
+        int firstTerm =1;
+        int counter =0;
+        while(counter < termNum-1){
+            lastTerm = tempTerm + firstTerm;
+            tempTerm=firstTerm;
+            firstTerm = lastTerm;
+            counter++;
+        }
+        return lastTerm;
+    }
+}
+
